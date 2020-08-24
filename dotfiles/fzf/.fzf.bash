ebcbbe81bce218c6810d7dbd6e67753adc1dd563
J Evans <10482250+jayeve@users.noreply.github.com>
2020-08-24 13:45:01 -0700

    fix fe() fzf shortcut git completions problem

--- dotfiles/fzf/.fzf.bash
+++ dotfiles/fzf/.fzf.bash
@@ -12,3 +12,36 @@ fi
 # ------------
 source "/usr/local/opt/fzf/shell/key-bindings.bash"
 
+export FZF_DEFAULT_COMMAND='rg -L --files --hidden'
+
+# fe [FUZZY PATTERN] - Open the selected file with the default editor
+#   - Bypass fuzzy finder if there's only one match (--select-1)
+#   - Exit if there's no match (--exit-0)
+fe() {
+  local __files
+  OLDIFS=$IFS
+  IFS=$'\n' __files=($(fzf-tmux --query="$1" --multi --select-1 --exit-0))
+  [[ -n "$__files" ]] && ${EDITOR:-vim} "${__files[@]}" && IFS=$OLDIFS
+}
+
+# fd [FUZZY PATTERN] - Open the selected folder
+#   - Bypass fuzzy finder if there's only one match (--select-1)
+#   - Exit if there's no match (--exit-0)
+fd() {
+  local __file
+  local __dir
+  __file=$(fzf +m -q "$1") && __dir=$(dirname "$__file") && cd "$__dir"
+}
+
+if [[ $(uname) == "Darwin" ]]; then
+  # dirname on OS X behaves funky, get gdirname via
+  # brew install coreutils
+  export dirname_command="gdirname"
+else
+  export dirname_command="dirname"
+fi
+
+# Faster compgen
+_fzf_compgen_dir() {
+  rg --hidden --files --null "$1" 2 > /dev/null | xargs -0 "$dirname_command" | awk '!h[$0]++'
+}
