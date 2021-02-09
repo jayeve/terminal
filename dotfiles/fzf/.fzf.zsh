cf963fa406c9d61ac2b4be5563eb06d9a81aadb0
J Evans <10482250+jayeve@users.noreply.github.com>
2021-02-09 11:19:12 -0800

    better fzf finding in shell

--- dotfiles/fzf/.fzf.zsh
+++ dotfiles/fzf/.fzf.zsh
@@ -12,3 +12,6 @@ fi
 # ------------
 [ -f /usr/local/opt/fzf/shell/key-bindings.zsh ] && source /usr/local/opt/fzf/shell/key-bindings.zsh
 [ -f /usr/share/doc/fzf/examples/key-bindings.zsh ] && source /usr/share/doc/fzf/examples/key-bindings.zsh
+
+export FZF_DEFAULT_COMMAND='rg -L --files --hidden'
+export FZF_CTRL_T_COMMAND=$FZF_DEFAULT_COMMAND
